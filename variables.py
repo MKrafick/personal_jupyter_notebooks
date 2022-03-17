48b00433ae3c69482f76d7b1250c72d1a5708318
Michael Krafick <michael.krafick@gmail.com>
2022-03-17 13:48:03 -0400

    Initial Commit

---
+++ variables.py
@@ -0,0 +1,24 @@
+#You will need to create a variable file (variable.py) for any connection
+#variable you are referencing in the Jupyter Notebook you are using.
+
+# Single line format looks like:
+#    VAR_Keyword = {
+#        'VARIABLE_OPTION':'hostname.com'
+#    }
+
+#    VAR_Keyword = {
+#        'DEV':'development.company.com'
+#    }
+
+#M ultiple line format looks like:
+#    WCS9_host = {
+#        'VARIABLE_OPTION_1':'hostname.com',
+#        'VARIABLE_OPTION_1':'hostname.com',
+#        'VARIABLE_OPTION_1':'hostname.com',
+#    }
+
+#    WCS9_host = {
+#        'DEV':'develop.company.com',
+#        'QA':'qa.company.coms',
+#        'PROD':'prod.company.com',
+#    }
